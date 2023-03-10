6a65ff2c6134fcad0f09af2c7aca08a4d28c5dfe
Yash Tilokani <yashtilokani375@gmail.com>
2023-03-10 13:55:10 +0530

    File and Folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import APP from "./components/App";
 
-ReactDOM.render(<APP />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+        <APP />
+    </BrowserRouter>,
+    document.getElementById("root")
+);
