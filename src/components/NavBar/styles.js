0dc8a0191f96df98aad43d7ab1732b5fff88a4a7
Yash Tilokani <yashtilokani375@gmail.com>
2023-03-10 18:04:02 +0530

    create Navbar and Sidebar

---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,37 @@
+import { makeStyles } from "@mui/styles";
+
+const drawerWidth = 240;
+
+export default makeStyles((theme) => ({
+    toolbar: {
+        height: "80px",
+        display: "flex",
+        justifyContent: "space-between",
+        marginLeft: "240px",
+        [theme.breakpoints.down('sm')]: {
+            marginLeft: 0,
+            flexWrap: 'wrap',
+        },
+    },
+    manuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    drawerpaper: {
+        width: drawerWidth,
+    },
+    linkButton: {
+        '&:hover': {
+            color: 'white !important',
+            TextDecoration: 'none',
+        },
+    },
+}));
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import APP from "./components/App";
 
+const theme = createTheme({});
+
 ReactDOM.render(
-    <BrowserRouter>
-        <APP />
-    </BrowserRouter>,
+    <ThemeProvider theme={theme}>
+        <BrowserRouter>
+            <APP />
+        </BrowserRouter>
+    </ThemeProvider>,
     document.getElementById("root")
 );
